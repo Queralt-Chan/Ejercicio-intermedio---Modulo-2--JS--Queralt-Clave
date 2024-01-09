8b6580deb9e05e839d79376a9dcff253f3da408e
queralt-chan <queraltbali@gmail.com>
2024-01-09 17:20:02 +0800

    add functions

--- main.js
+++ main.js
@@ -3,3 +3,22 @@
 const playOptions = document.querySelector ('.js-options');
 const playButton = document.querySelector ('.js-btn')
 const playMessage = document.querySelector ('.js-message')
+
+function getRandomNumber(max) { 
+    return Math.ceil(Math.random() * max); 
+  } 
+
+  
+function handleClick (event){
+    event.preventDefault ();
+    const Options = playOptions.value; 
+    console.log (Options);
+   
+}
+
+
+playButton.addEventListener ('click', handleClick);
+
+
+
+
