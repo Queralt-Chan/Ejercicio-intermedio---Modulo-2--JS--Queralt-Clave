82b9915effa8fea294523c569231973e13e865b8
queralt-chan <queraltbali@gmail.com>
2024-01-09 16:31:29 +0800

    html

---
+++ main.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const playOptions = document.querySelector ('.js-options');
+const playButton = document.querySelector ('.js-btn')
+const playMessage = document.querySelector ('.js-message')
