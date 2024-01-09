164c8fd3e104a61224fbcb4cc8ac0336f71eff19
queralt-chan <queraltbali@gmail.com>
2024-01-09 18:17:38 +0800

    last

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const playOptions = document.querySelector ('.js-options');
-const playButton = document.querySelector ('.js-btn')
-const playMessage = document.querySelector ('.js-message')
+const playButton = document.querySelector ('.js-btn');
+const playMessage = document.querySelector ('.js-message');
+const stone = document.querySelector ('.js-stone');
+const paper = document.querySelector ('.js-paper');
+const scissor = document.querySelector ('.js-scissor');
 
 function getRandomNumber(max) { 
     return Math.ceil(Math.random() * max); 
   } 
+function test();
+ 
+    if (getRandomNumber <=3) {
+
+    }
 
   
 function handleClick (event){
